796f383ddba8504b22e8c5c3cfaef14cfb6c1811
zondahuman <zondahuman@gmail.com>
2016-09-15 23:47:27 +0800

    add

---
+++ helix-service/src/main/java/com/abin/lee/helix/service/DistributeSystemManage.java
@@ -0,0 +1,11 @@
+package com.abin.lee.helix.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: abin
+ * Date: 16-9-15
+ * Time: 下午11:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class DistributeSystemManage {
+}
